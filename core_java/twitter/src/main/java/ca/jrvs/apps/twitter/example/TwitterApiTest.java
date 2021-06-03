f2202819e051907bda961fe1213c70cebb1207df
akirilenko200 <83600204+akirilenko200@users.noreply.github.com>
2021-06-02 18:28:17 -0600

    Update TwitterApiTest.java

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/example/TwitterApiTest.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/example/TwitterApiTest.java
@@ -15,14 +15,11 @@ import org.apache.http.util.EntityUtils;
 
 public class TwitterApiTest {
 
-//  private static String CONSUMER_KEY = System.getenv("cosumerKey");
-//  private static String CONSUMER_SECRET = System.getenv("consumerSecret");
-//  private static String ACCESS_TOKEN = System.getenv("accessToken");
-//  private static String TOKEN_SECRET = System.getenv("cosumerKey");
-  private static String CONSUMER_KEY = "69poB0utiGYgY7XwYlAfhMZ71";
-  private static String CONSUMER_SECRET = "QjrijragoMvJS9HVEj4cTVEdo3RCYxSrqfr2MEXOGOzXmqy17q";
-  private static String ACCESS_TOKEN = "1394855368379965443-rJMg114OFyuCdXiLLDpzgnx20KLT6n";
-  private static String TOKEN_SECRET = "3IPO5ntPhd3VwgzYG1XSz9ZH2NQBEFXUiyhlwhF0vpWl0";
+  private static String CONSUMER_KEY = System.getenv("cosumerKey");
+  private static String CONSUMER_SECRET = System.getenv("consumerSecret");
+  private static String ACCESS_TOKEN = System.getenv("accessToken");
+  private static String TOKEN_SECRET = System.getenv("cosumerKey");
+
 
   public static void main(String[] args)
       throws Exception {
